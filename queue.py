bc432ad3693a1260f20f33169d010162d3373d43
Connor Noble <noblecp1@gmail.com>
2022-10-01 00:03:50 -0400

    filled out queue structure

--- queue.py
+++ queue.py
@@ -1,3 +1,14 @@
-class Queue:
-    def __init__(self):
-        self.queue = ""
+from collections import deque
+
+'''
+A queue can implemented in Python using either...
+- a list
+- collections.deque object
+- queue.Queue object
+
+Basic operations include:
+        - enequeue() -> Adds an item to the queue
+        - dequeue() -> Removes an element from the queue in order they were added (FiFo)
+        - front() -> Returns the first (front) item in the queue
+        - rear() -> Returns the last item from the queue
+'''
